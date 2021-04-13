bab6d5288875aa276acbb629b0f850f429ae7c7f
albertkhudaev <albert-khudaev@yandex.ru>
2021-04-14 00:57:30 +0500

    Добавил решение для третьего задания

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+cd ../ask
+gunicorn --bind='0.0.0.0:8080' ask.wsgi:application
--- testserver.py
+++ testserver.py
@@ -1,8 +1,9 @@
 import wsgiref
 from wsgiref.simple_server import make_server, demo_app
 from hello import app
+from ask.ask.wsgi import application
 
 
-with make_server('localhost', 8000, app) as httpd:
+with make_server('localhost', 8000, application) as httpd:
     print("server HTTP on port 8000")
     httpd.serve_forever()
