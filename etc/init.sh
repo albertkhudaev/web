7932caf29281bddea05be668720df0001d2fae25
albertkhudaev <albert-khudaev@yandex.ru>
2021-04-14 09:50:47 +0500

    Поправил порт gunicorn

--- etc/init.sh
+++ etc/init.sh
@@ -2,4 +2,4 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd ../ask
-gunicorn --bind='0.0.0.0:8080' ask.wsgi:application
+gunicorn --bind='0.0.0.0:8000' ask.wsgi:application
