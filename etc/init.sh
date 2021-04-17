1c5307f53f9a93f10f5c5dd1fc82c57a1b6260b2
albertkhudaev <albert-khudaev@yandex.ru>
2021-04-17 21:37:48 +0500

    Добавил выполнение третьего задания

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,9 @@
+sudo apt update
+sudo apt install python3.5 
+sudo rm /usr/bin/python3   
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3 
+sudo pip3 install gunicorn 
+sudo pip3 install django==2.1  
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
